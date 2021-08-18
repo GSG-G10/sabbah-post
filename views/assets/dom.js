357850cdb0c835625084a81a0627783365337ccc
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-18 19:42:07 +0300

    build data in DOM

--- views/assets/dom.js
+++ views/assets/dom.js
@@ -1,20 +1,26 @@
-// const submit = document.querySelector('#submit');
-// const nameInput = document.querySelector('#name');
-// const postInput = document.querySelector('#post-text');
-// const imgInput = document.querySelector('#img-url');
+const createEle = (tag, className, parent, text) => {
+    const element = document.createElement(tag)
+    element.classList.add(className)
+    parent.appendChild(element)
+    if (tag === 'img'){
+        element.src = tag
+    }else{
 
-// const postData = () => {
-//   if (nameInput.value && postInput.value) {
-//     fetch('/post', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//          name: nameInput.value,
-//           postText: postInput.value,
-//         imgUrl: imgInput.value }),
-//     }, method = 'POST')
-//   }
-// }
+        element.textContent = text;
+    }
+    return element
+}
+const postsSecion = document.querySelector('.posts-present');
+const appenData = (arr) => {
+    for (let i = 0; i < arr.length; i += 1) {
+        const name = arr[i].user_name;
+        const postText = arr[i].post_text;
+        const imgUrl = arr[i].link_img;
+        const container = createEle('div', 'post-container', postsSecion, '');
+        createEle('h2', 'h2', container, name)
+        createEle('p', 'p', container, postText)
+        createEle('div', 'img-cont', container, '')
+        createEle('img', 'img', container, imgUrl)
 
+    }
+}
