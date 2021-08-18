284c8391d5cbc8b3942dce3e8754da101c4c60a9
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-18 17:44:06 +0300

    connecting to the data base and get data from it Relates #7

--- server/database/config/init.sql
+++ server/database/config/init.sql
@@ -6,10 +6,9 @@ DROP TABLE IF EXISTS posts CASCADE ;
 CREATE TABLE posts(
 id BIGSERIAL PRIMARY KEY NOT NULL,
 user_name VARCHAR(100) NOT NULL,
-text VARCHAR(500) NOT NULL,
-link_img text NOT NULL,
-likes VARCHAR(10) NOT NULL);
+post_text VARCHAR(500) NOT NULL,
+link_img text);
 
-INSERT INTO posts (user_name, text, link_img, likes) VALUES ('ahmad' , 'this is text default about  this image', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' , 540);
+INSERT INTO posts (user_name, post_text, link_img) VALUES ('ahmad' , 'this is text default about  this image', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png');
 
 COMMIT;
